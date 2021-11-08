ddf7fd1b7cf9493adf74bd0a3e4f52e73cca3718
CHIEN HAO TSENG <stan001182@gmail.com>
2021-11-09 01:15:54 +0800

    加上簡單地圖

---
+++ Dream Trip/MapViewController1.swift
@@ -0,0 +1,51 @@
+//
+//  MapViewController1.swift
+//  Dream Trip
+//
+//  Created by Stan on 2021/11/9.
+//
+
+import UIKit
+import CoreLocation
+
+class MapViewController1: UIViewController, CLLocationManagerDelegate {
+
+    let lm = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        lm.requestWhenInUseAuthorization()
+        lm.requestAlwaysAuthorization()
+        
+        lm.delegate = self
+        lm.startUpdatingLocation()
+                
+            }
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+                if let location = locations.first{
+                    print("經度：\(location.coordinate.longitude)")
+                    print("緯度：\(location.coordinate.latitude)")
+                    print("高度：\(location.altitude)")
+    
+        lm.stopUpdatingLocation()
+        
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
